0818902d8d1d0d9c792b77a6a171ba04cdfeba0e
Maione Ribeiro <cunhamaione@gmail.com>
2025-03-19 16:59:38 -0300

    Estudos

---
+++ lacos.py
@@ -0,0 +1,16 @@
+'''Quando sabe o limite usa o for ou while, quando não sabe usa somente o while.'''
+
+for c in range(1, 10): 
+    print(c)
+      
+print("Fim!")
+
+
+
+n = 1
+while n != 0:
+   n = int(input("Digite um valor: "))
+print("fim")
+
+
+
