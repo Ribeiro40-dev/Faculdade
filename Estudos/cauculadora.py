c1dd3dbb334e27266da7a45f5f427bbac883311c
Maione Ribeiro <cunhamaione@gmail.com>
2025-04-04 10:14:40 -0300

    ex

---
+++ Estudos/cauculadora.py
@@ -0,0 +1,9 @@
+n1 = int(input("Digite um número: "))
+n2 = int(input("Digite outro número: "))
+
+s = n1 + n2
+print(s)
+
+switch:
+case:
+    
--- Estudos/test.py
+++ Estudos/test.py
@@ -1,2 +1,15 @@
+'''msm = input("Qual é o seu nome! ")
 
-print("hello words! ")
+print("Seja bem vindo! ",msm)
+
+
+print("Hello words", end="!\n")
+
+x = int(input("x: "))
+
+y = int(input("y: "))
+
+print(x + y)'''
+
+
+
